422847d8b2236bbae90ca7378e22823c558ec4f0
pedrocanute <pedro.canute2@gmail.com>
2026-04-20 10:29:32 -0300

    Criada a classe estatica TratarRetorno

---
+++ SistemaAutonomo/TratarRetorno.cs
@@ -0,0 +1,13 @@
+﻿public static class TratarRetorno
+{
+    public static string[] SepararLinha(string retorno)
+    {
+        retorno = retorno.Replace("\r", "");
+        return retorno.Split('\n');
+    }
+    public static string[] SepararVirgula(string retorno)
+    {
+        retorno = retorno.Replace("\r", "");
+        return retorno.Split(',');
+    }
+}
