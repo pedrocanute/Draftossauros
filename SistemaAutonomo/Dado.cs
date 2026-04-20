6b307e26707d66f5e722a8bfeaddbdab7256d27c
MTada <mtadashibr215@gmail.com>
2026-04-20 11:37:50 -0300

    Classe dado criada

---
+++ SistemaAutonomo/Dado.cs
@@ -0,0 +1,18 @@
+﻿public class Dado
+{
+    string face;
+
+    public string Face
+    {
+        get { return face; }
+        set { face = value; }
+    }
+
+
+    public Dado(string face)
+    {
+        this.face = face;
+    }
+}
+
+
